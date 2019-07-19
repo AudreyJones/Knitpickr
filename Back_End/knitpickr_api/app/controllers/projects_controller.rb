bc46e4f124b691b05087e4b5ede65c72f95d3f39
AudreyJones <audrey.eliz.jones@gmail.com>
2019-07-19 11:25:36 -0400

    Saving?

--- Back_End/knitpickr_api/app/controllers/projects_controller.rb
+++ Back_End/knitpickr_api/app/controllers/projects_controller.rb
@@ -1,2 +1,7 @@
 class ProjectsController < ApplicationController
+    # def index
+    #     conn = Faraday::Connection.new 'RAVELRY_API'
+    #     @data = conn.get 
+    #     render json: JSON.parse(@data.body)
+    # end
 end
