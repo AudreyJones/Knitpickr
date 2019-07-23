f983cb17c60f42d405c4bb15e5ca7eda40665269
AudreyJones <audrey.eliz.jones@gmail.com>
2019-07-23 16:51:01 -0400

    Safety save 7.23.19

--- Front_End/knitpickr/src/components/ProjectShow.js
+++ Front_End/knitpickr/src/components/ProjectShow.js
@@ -8,7 +8,7 @@ export default function ProjectShow(props) {
     return(
         <div>
         <h1>Project: {props.name}</h1>
-        <h2>Favorited? </h2>
+        <h2>Favorited? {props.favorited ? true : null}</h2>
         <h2>You will need: 
             {props.materials.length > 0 ? props.materials.map((material) => <MaterialCard key={material.id} material={material} />): "No materials available for this project"}
 
